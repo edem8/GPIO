57d0d88bb1b34b884729aaefa5d0640b13f97adc
edem8 <eddieblack159@gmail.com>
2023-08-26 19:44:39 +0100

    pretty printing

--- artistInfo.py
+++ artistInfo.py
@@ -8,21 +8,24 @@ button = Button(2)
 API_KEY = "6bbb512da09f63739a8b648f1e908cd9" 
 ARTIST = input("Who is your favourite Artst?: ")
 
-print(f"Press the button to find out about {ARTIST}")
+print(f"\n...Press the button to find out about {ARTIST}...\n")
 
-url =  "http://ws.audioscrobbler.com/2.0/"
-
-params = {
-
-    "method": "artist.getinfo",
-    "artist": ARTIST,
-    "api_key": API_KEY,
-    "format": "json"
-}
 
 def printInfo():
+	url =  "http://ws.audioscrobbler.com/2.0/"
+
+	params = {
+    	"method": "artist.getinfo",
+    	"artist": ARTIST,
+    	"api_key": API_KEY,
+    	"format": "json"
+	}
+
 	response = requests.get(url, params=params)
-	pprint(response.json()["artist"]["bio"]["content"])
+
+	info = response.json()["artist"]["bio"]["content"]
+	print(info.rstrip("\n"), end="")
+	print()
 
 
 printInfo()
